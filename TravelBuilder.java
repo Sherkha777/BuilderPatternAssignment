b6db30f569b8b219e4179caf6d59fc10ab172b7b
Sherkhan <ngjzty2d56@privaterelay.appleid.com>
2026-09-11 01:41:16 +0500

    The interface

---
+++ TravelBuilder.java
@@ -0,0 +1,6 @@
+public interface TravelBuilder {
+   TravelBuilder setDestination(String destination);
+   TravelBuilder setAccommodation(String accommodation);
+   TravelBuilder setTransport(String transport);
+   TravelBuilder setDuration(int days);
+}
